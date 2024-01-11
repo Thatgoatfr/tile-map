4edff10fd0a2688b04fef2e717fa83b0cf706d69
Thatgoatfr <150864315+Thatgoatfr@users.noreply.github.com>
2024-01-11 12:19:13 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+tiles.setCurrentTilemap(tilemap`level1`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 3 3 . . . . . . . . . 
+    . . . . . . . 3 3 3 . . . . . . 
+    . . . . . . . . . . 3 3 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
